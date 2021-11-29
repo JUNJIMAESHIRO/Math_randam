e68d503207b262f0a174fd11fee219031eff8793
MaeshiroJunji <junjimaeshiro0613@gmail.com>
2021-11-29 18:40:10 +0900

    html csssの編集

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+$(function(){
+  var count = 0;
+  var ranmath = Math.floor( Math.random() * 11 ) ;
+  $("#mathbutton").on("click",function(){
+    count = count+1;
+    let math = $("#text").val()
+    console.log(math);
+    if (math == ranmath) {
+      let farst = $(".farst_results")
+      var answer1 = "Awesome"
+      farst.text(answer1)
+      let total = $(".totaltaime")
+      var totalcount = "This time you hit it with in "+ count + " time"
+      total.text(totalcount)
+    }else if (math < ranmath){
+      let farst = $(".farst_results")
+      var answer2 = "Higher"
+      farst.text(answer2)
+    }else if (math > ranmath){
+      let farst = $(".farst_results")
+      var answer3 = "Lower"
+      farst.text(answer3)
+    }
+  })
+})
